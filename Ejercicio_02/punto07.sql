12543c8021080372910f2928f9f98bcf0f7198e4
Enzo <enzo7agustin@gmail.com>
2025-12-12 11:13:25 -0300

    corregido

--- Ejercicio_02/punto07.sql
+++ Ejercicio_02/punto07.sql
@@ -1,11 +1,26 @@
--- punto 7
+SELECT especie 
+FROM Arbol A 
+INNER JOIN Localidad L ON A.codigoPostal = L.codigoPostal 
+WHERE L.nombreL = 'La Plata'
 
-SELECT especie
-FROM ARBOL a
-WHERE EXISTS (
-	-- Arboles en La Plata
-	SELECT * FROM Localidad
-	WHERE A.codigoPostal = Localidad.codigoPostal 
-	AND (Localidad.nombreL = 'La Plata' OR Localidad.nombreL = 'Salta')
+INTERSECT
 
-)
+SELECT especie 
+FROM Arbol A 
+INNER JOIN Localidad L ON A.codigoPostal = L.codigoPostal 
+WHERE L.nombreL = 'Salta';
+
+
+-- Otra solución sin INTERSECT
+
+SELECT DISTINCT especie 
+FROM Arbol A
+INNER JOIN Localidad L ON A.codigoPostal = L.codigoPostal
+WHERE L.nombreL = 'La Plata'
+AND A.especie IN (
+    SELECT especie 
+    FROM Arbol A2
+    INNER JOIN Localidad L2 ON A2.codigoPostal = L2.codigoPostal
+    WHERE L2.nombreL = 'Salta'
+);
+);
