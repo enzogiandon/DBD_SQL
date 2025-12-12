656bace5475d1d599bced29a548fdea4aa364531
Enzo Giandon <enzogiandon@alu.ing.unlp.edu.ar>
2025-12-12 18:49:52 -0300

    Update punto04.sql

--- Ejercicio_03/punto04.sql
+++ Ejercicio_03/punto04.sql
@@ -1,1 +1,20 @@
 -- Punto 04
+SELECT I.DNI, I.nombre, I.apellido, I.email
+FROM Integrante I
+
+WHERE EXISTS (
+-- Integrantes que tocaron en Carlos Gardel
+	SELECT *
+	FROM RECITAL R
+	INNER JOIN RECITAL R ON I.codigoB = R.codigoB
+	INNER JOIN Escenario E ON R.nroEscenario = E.nroEscenario
+	WHERE E.nombre_escenario = 'Gustavo Cerati' 
+)
+AND EXISTS (
+-- Integrantes que tocaron en Carlos Gardel
+	SELECT *
+	FROM RECITAL R
+	INNER JOIN RECITAL R ON I.codigoB = R.codigoB
+	INNER JOIN Escenario E ON R.nroEscenario = E.nroEscenario
+	WHERE E.nombre_escenario = 'Carlos Gardel'
+)
